1a2a93d375caf18c36475c42fc4490454ddf9170
lyh <lyhian2020@gmail.com>
2021-09-24 12:21:19 +0800

    f1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def function1():
-    print('This text represent the content of function 1')  #To be edited by member 1, Replace this line with your actual function code
+    print("This is ENG1003's first Tuturial Programing Task \nEnter the fuction to be excute:")
+    print("YAN\nHim\nIan\nWalton\nRonald")
     
 
 def function2():
