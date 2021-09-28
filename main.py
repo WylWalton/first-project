4daa80a2e007f92ea83c4d05469b829757537752
lyh <lyhian2020@gmail.com>
2021-09-28 23:29:13 +0800

    debug1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def function2():
 
 def function3():
     a = 5 
-    print('a = '+str(a)+'the quarter of a is '+str(a*a))
+    print('a = '+str(a)+' , Square of a is '+str(a*a))
 
 def function4():
     x = 6 
